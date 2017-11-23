aca073ada0a0c8498ead339fe254ad42ab47f83d
Vins <v.plantone@studenti.uniba.it>
2017-11-23 22:23:10 +0100

    Test appello 2.0

--- AppelloParcheggi/src/exceptions/IsFullException.java
+++ AppelloParcheggi/src/exceptions/IsFullException.java
@@ -10,7 +10,7 @@ public class IsFullException extends Exception {
 	/**
 	 * 
 	 */
-	private static final long SERIALVERSIONUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @constructor
